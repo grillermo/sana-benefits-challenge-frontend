79f90b4d83e4df10baefed938fe58924ac206562
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2021-12-24 16:14:18 -0600

    Configure Jest with Enzyme
    And prove it works by testing the root component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let tree, props;
+
+  const buildTree = (newProps = {}) => {
+    return shallow(<App />);
+  };
+
+  test('renders successfully', () => {
+    tree = buildTree();
+    expect(tree).toMatchSnapshot();
+  });
 });
